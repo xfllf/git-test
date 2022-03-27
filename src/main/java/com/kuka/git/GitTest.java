0a8c4a602777379e14bf794e55d026749aed5556
kuka_lif <3123825833@qq.com>
2022-03-27 16:57:03 +0800

    second commit

--- src/main/java/com/kuka/git/GitTest.java
+++ src/main/java/com/kuka/git/GitTest.java
@@ -5,5 +5,6 @@ import com.sun.corba.se.impl.resolver.SplitLocalResolverImpl;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
