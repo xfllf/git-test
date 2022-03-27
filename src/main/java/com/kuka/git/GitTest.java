4a985ac249867e26197a60bf24261c5ee960b1cf
kuka_lif <3123825833@qq.com>
2022-03-27 16:54:54 +0800

    first commit

---
+++ src/main/java/com/kuka/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.kuka.git;
+
+import com.sun.corba.se.impl.resolver.SplitLocalResolverImpl;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
